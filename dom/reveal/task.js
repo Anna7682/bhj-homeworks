11159bebfcc66bbfcf01809a2b158dc7fda33b90
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2022-11-21 20:36:06 +0300

    task completed

---
+++ dom/reveal/task.js
@@ -0,0 +1,14 @@
+const reveal = document.querySelectorAll('.reveal'),
+    windowHeight = window.innerHeight;
+
+reveal.forEach((item) => {
+    document.addEventListener('scroll', () => {
+        if (item.getBoundingClientRect().top < windowHeight && item.getBoundingClientRect().top + item.getBoundingClientRect().height > 0) {
+            item.classList.add('reveal_active');
+        } else {
+            item.classList.remove('reveal_active');
+        }
+    });
+});
+Footer
+© 2022 GitHub, Inc.
