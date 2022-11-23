fddd48c9d8eef43ece8915bcfe660d7aef67c7b0
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2022-11-23 20:47:22 +0300

    New file

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,52 +1,14 @@
-const timer = document.getElementById('timer');
-let countdown = timer.textContent;
-const timerInterval = setInterval(() => {
-    countdown -= 1;
-    timer.textContent = countdown;
+const cookie = document.getElementById('cookie'),
+      clickCounter = document.getElementById('clicker__counter'),
+      clickSpeed = document.getElementById('clicker__speed');
+let timerStart = new Date;
 
-    if (countdown <= 0) {
-        alert('Вы победили в конкурсе!');
-        clearInterval(timerInterval);
-    }
-}, 1000);
+cookie.onclick = function () {
+    clickCounter.textContent = parseInt(clickCounter.textContent) + 1;
+    cookie.width = cookie.width != 200 ? 200 : 180;
 
-// Дополнительная задача #1
-const wireTime = document.getElementById('wiretimer');
-const timerSplit = wireTime.textContent.split(':');
-let countdownExtended = timerSplit[0] * 60 * 60 + timerSplit[1] * 60 + timerSplit[2] * 1;
-
-const wireTimerInterval = setInterval(() => {
-    countdownExtended -= 1;
-    wireTime.textContent = secToString(countdownExtended);
-
-    if (countdownExtended <= 0) {
-        alert('Всё взорвалось :(');
-        clearInterval(wireTimerInterval);
-    }
-}, 1000);
-
-function secToString(sec) {
-    let hours = Math.floor(sec / (60 * 60)),
-        minutes = Math.floor(sec / 60 % 60),
-        seconds = Math.floor(sec % 60);
-
-    hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-
-    return hours + ':' + minutes + ':' + seconds;
+    const timerNext = new Date;
+    let speed = timerNext.getTime() - timerStart.getTime();
+    clickSpeed.textContent = (1 / (speed / 1000)).toFixed(2);
+    timerStart = new Date;
 }
-
-// Дополнительная задача #2
-const fileLink = document.getElementById('filelink');
-const filetimer = document.getElementById('filetimer');
-let countdownFile = filetimer.textContent;
-const fileTimerInterval = setInterval(() => {
-    countdownFile -= 1;
-    filetimer.textContent = countdownFile;
-
-    if (countdownFile <= 0) {
-        fileLink.click();
-        clearInterval(fileTimerInterval);
-    }
-}, 100);
