0c465a75d1c1321c0de008d52cdaefc1b531a7fb
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2022-12-14 18:53:30 +0300

    Upload file

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -2,43 +2,34 @@ const pollTitle = document.getElementById('poll__title'),
     pollAnswers = document.getElementById('poll__answers');
 
 let xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/poll.php');
+xhr.open('GET', 'https://test-back-ir02.onrender.com/poll');
 xhr.send();
-xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === xhr.DONE && xhr.status == 200) {
-
-        const ansXhr = JSON.parse(xhr.responseText);
-
-        pollTitle.innerText = ansXhr.data.title;
-
-        for (let answer in ansXhr.data.answers) {
-            const ansElement = document.createElement('button');
-            ansElement.className = 'poll__answer';
-            ansElement.innerText = ansXhr.data.answers[answer];
-
-            ansElement.addEventListener('click', () => {
+xhr.addEventListener('readystatechange', ()=>{
+    if(xhr.readyState == xhr.DONE) {
+        const parse = JSON.parse(xhr.responseText);
+        poll__title.insertAdjacentHTML('afterbegin', `${parse.data.title}`);
+        for (let i = 0; i < parse.data.answers.length; i++) {
+            poll__answers.insertAdjacentHTML('beforeend', `<button class="poll__answer">${parse.data.answers[i]}</button>`);
+            document.getElementsByClassName('poll__answer')[document.getElementsByClassName('poll__answer').length - 1].addEventListener('click', () => {
                 alert('Спасибо, ваш голос засчитан!');
 
-                // отображение результатов
-                xhr = new XMLHttpRequest();
-                xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/poll.php');
-                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                xhr.send(`vote=${ansXhr.id}&answer=${answer}`);
-                xhr.addEventListener('readystatechange', () => {
-                    if (xhr.readyState === xhr.DONE && xhr.status == 200) {
-                        const resXhr = JSON.parse(xhr.responseText);
-                        const resSummary = Object.values(resXhr.stat).reduce((sum, item) => sum + item.votes, 0); // считаем сумму всех голосов
-
-                        pollAnswers.innerHTML = ''; // убираем кнопки голосования
-                        for (let result in resXhr.stat) {
-                            pollAnswers.innerHTML += `<div>${resXhr.stat[result].answer}: <b>${(resXhr.stat[result].votes / resSummary * 100).toFixed(2)}%</b></div>`;
+                let xhr_second = new XMLHttpRequest();
+                xhr_second.open( 'POST', 'https://test-back-ir02.onrender.com/poll' );
+                xhr_second.setRequestHeader( 'Content-type', 'application/x-www-form-urlencoded' );
+                xhr_second.send( `vote=${parse.id}&answer=${i}` );
+                xhr_second.addEventListener('readystatechange', ()=>{
+                    if(xhr_second.readyState == xhr_second.DONE) {
+                        const parse_second = JSON.parse(xhr_second.responseText);
+                        while (document.getElementsByClassName('poll__answer').length) {
+                            document.getElementsByClassName('poll__answer')[0].remove();
+                        }
+                       
+                        for (let j = 0; j < parse_second.stat.length; j++) {
+                            poll__answers.insertAdjacentHTML('beforeend', `<div>${parse_second.stat[j].answer}: ${parse_second.stat[j].votes}</div>`);
                         }
                     }
                 });
-
             });
-
-            pollAnswers.appendChild(ansElement);
         }
     }
 });
